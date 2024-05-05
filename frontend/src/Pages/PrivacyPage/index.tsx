b7fd47f936f8af5477690abb58bc3f23a1804d1d
Dane Urban <z5363301@ad.unsw.edu.au>
2024-05-05 16:25:52 +1000

    [+] Styling

--- frontend/src/Pages/PrivacyPage/index.tsx
+++ frontend/src/Pages/PrivacyPage/index.tsx
@@ -1,106 +1,109 @@
 const PrivacyPage = () => {
     return (
         <div>
-            <div>
-                <h1>Privacy Policy</h1>
+            <div className="text-center text-white">
+                <h1 className="pb-4">
+                    Privacy Policy
+                </h1>
+                <div>
+                    Examify ("we", "us", or "our") is committed to protecting the privacy of our users. This Privacy Policy outlines how we collect, use, disclose, and safeguard your information when you use our software-as-a-service platform and website located at [YourWebsite.com] (collectively, the "Service"). Please read this Privacy Policy carefully. By accessing or using our Service, you agree to be bound by this Privacy Policy.
 
-                Examify ("we", "us", or "our") is committed to protecting the privacy of our users. This Privacy Policy outlines how we collect, use, disclose, and safeguard your information when you use our software-as-a-service platform and website located at [YourWebsite.com] (collectively, the "Service"). Please read this Privacy Policy carefully. By accessing or using our Service, you agree to be bound by this Privacy Policy.
-
-                <ol>
-                    <li>
-                        Information We Collect
-                        <ul>
-                            <li>
-                                We may collect personal information that you provide to us when you register for an account, use our Service, or communicate with us. This information may include:
-                                <br />
-                                Personal identification information, such as your name, email address, phone number, and other contact details.
-                                User credentials, such as your username and password.
-                                Information you provide when you create or update your account profile.
-                                Payment information, such as credit card details, for processing payments.
-                                Communications and correspondence exchanged with us, including support inquiries and feedback.
-                                Additionally, we may automatically collect certain information about your device and usage of the Service, such as your IP address, browser type, operating system, referral URLs, and usage patterns.
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        Use of Information
-                        <ul>
-                            <li>
-                                We may use the information we collect for various purposes, including:
-                                <br />
-                                Providing, maintaining, and improving the Service.
-                                Customizing and personalizing your experience with the Service.
-                                Processing transactions and payments.
-                                Responding to your inquiries, requests, and feedback.
-                                Communicating with you about updates, promotions, and news related to the Service.
-                                Monitoring and analyzing trends, usage, and activities in connection with the Service.
-                                Detecting, preventing, and addressing technical issues, security breaches, and fraudulent activities.
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                    Sharing of Information
-                        <ul>
-                            <li>
-                            We may share your information with third parties in the following circumstances:
-                                <br />
-                                With your consent or at your direction.
-                With service providers, contractors, and business partners who assist us in operating, maintaining, and improving the Service.
-                To comply with legal obligations or enforce our rights.
-                In connection with a merger, acquisition, or sale of assets.
-                We may also aggregate or anonymize your information to create statistical data for analytical purposes, which may be shared with third parties.
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        Data Security
-                        <ul>
-                            <li>
-                            We take reasonable measures to protect the security of your information and prevent unauthorized access, disclosure, alteration, or destruction. However, please note that no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                    Your Choices
-                        <ul>
-                            <li>
-                            You may choose not to provide certain information, but this may limit or affect your ability to use certain features of the Service. You can also review and update your account information through your account settings.
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                    Third-Party Links
-                        <ul>
-                            <li>
-                            The Service may contain links to third-party websites or services that are not owned or controlled by us. We are not responsible for the privacy practices or content of such third-party sites and encourage you to review their privacy policies.
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                    Children's Privacy
-                        <ul>
-                            <li>
-                            The Service is not intended for children under the age of 13, and we do not knowingly collect personal information from children under 13. If you are a parent or guardian and believe that your child has provided us with personal information, please contact us so that we can take appropriate action.
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                    Changes to this Privacy Policy
-                        <ul>
-                            <li>
-                            We may update this Privacy Policy from time to time, and any changes will be posted on this page. Your continued use of the Service after such modifications will constitute your acknowledgment of the modified Privacy Policy and agreement to abide and be bound by the modified Privacy Policy.
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                    Contact Us
-                        <ul>
-                            <li>
-                            If you have any questions, concerns, or comments about this Privacy Policy or our privacy practices, please contact us at danelegend13@gmail.com.
-                            </li>
-                        </ul>
-                    </li>
-                </ol>
+                    <ol>
+                        <li>
+                            Information We Collect
+                            <ul>
+                                <li>
+                                    We may collect personal information that you provide to us when you register for an account, use our Service, or communicate with us. This information may include:
+                                    <br />
+                                    Personal identification information, such as your name, email address, phone number, and other contact details.
+                                    User credentials, such as your username and password.
+                                    Information you provide when you create or update your account profile.
+                                    Payment information, such as credit card details, for processing payments.
+                                    Communications and correspondence exchanged with us, including support inquiries and feedback.
+                                    Additionally, we may automatically collect certain information about your device and usage of the Service, such as your IP address, browser type, operating system, referral URLs, and usage patterns.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            Use of Information
+                            <ul>
+                                <li>
+                                    We may use the information we collect for various purposes, including:
+                                    <br />
+                                    Providing, maintaining, and improving the Service.
+                                    Customizing and personalizing your experience with the Service.
+                                    Processing transactions and payments.
+                                    Responding to your inquiries, requests, and feedback.
+                                    Communicating with you about updates, promotions, and news related to the Service.
+                                    Monitoring and analyzing trends, usage, and activities in connection with the Service.
+                                    Detecting, preventing, and addressing technical issues, security breaches, and fraudulent activities.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                        Sharing of Information
+                            <ul>
+                                <li>
+                                We may share your information with third parties in the following circumstances:
+                                    <br />
+                                    With your consent or at your direction.
+                    With service providers, contractors, and business partners who assist us in operating, maintaining, and improving the Service.
+                    To comply with legal obligations or enforce our rights.
+                    In connection with a merger, acquisition, or sale of assets.
+                    We may also aggregate or anonymize your information to create statistical data for analytical purposes, which may be shared with third parties.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            Data Security
+                            <ul>
+                                <li>
+                                We take reasonable measures to protect the security of your information and prevent unauthorized access, disclosure, alteration, or destruction. However, please note that no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                        Your Choices
+                            <ul>
+                                <li>
+                                You may choose not to provide certain information, but this may limit or affect your ability to use certain features of the Service. You can also review and update your account information through your account settings.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                        Third-Party Links
+                            <ul>
+                                <li>
+                                The Service may contain links to third-party websites or services that are not owned or controlled by us. We are not responsible for the privacy practices or content of such third-party sites and encourage you to review their privacy policies.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                        Children's Privacy
+                            <ul>
+                                <li>
+                                The Service is not intended for children under the age of 13, and we do not knowingly collect personal information from children under 13. If you are a parent or guardian and believe that your child has provided us with personal information, please contact us so that we can take appropriate action.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                        Changes to this Privacy Policy
+                            <ul>
+                                <li>
+                                We may update this Privacy Policy from time to time, and any changes will be posted on this page. Your continued use of the Service after such modifications will constitute your acknowledgment of the modified Privacy Policy and agreement to abide and be bound by the modified Privacy Policy.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                        Contact Us
+                            <ul>
+                                <li>
+                                If you have any questions, concerns, or comments about this Privacy Policy or our privacy practices, please contact us at danelegend13@gmail.com.
+                                </li>
+                            </ul>
+                        </li>
+                    </ol>
+                </div>
             </div>
         </div>
     )

2edc7133039820e6e45223fee62c245aceb97df4
Dane Urban <z5363301@ad.unsw.edu.au>
2024-07-16 23:36:31 +1000

    [+] Added feature to landing page

--- frontend/src/Pages/AboutPage/Components/Features.tsx
+++ frontend/src/Pages/AboutPage/Components/Features.tsx
@@ -1,170 +1,141 @@
 import { useEffect, useState } from "react"
+import backgroundImage from '../../../images/background-features.jpg'
+
+import examsImage from '../../../images/screenshots/exams.jpg'
+import questionsImage from '../../../images/screenshots/questions.jpg'
 
 type Feature = {
     title: string,
-    description: string
+    description: string,
+    image: string
 }
 
-/*
 const Features = () => {
     const features: Feature[] = [
         {
-            title: "Exam Finding",
-            description: "We know how hard it can be looking for practice exams so we provide the largest collection of HSC and Trial exams. Our filtering features help you find what you want."
+          title: "Past Exams",
+          description: "The largest collection of previous HSC exams and past trial papers at your fingertips",
+          image: examsImage
         },
         {
-            title: "Analytics",
-            description: "Wanting to know how you're progressing. We have you covered with analytics letting you know how your progression is coming along"
+          title: "Practice Questions",
+          description: "An extensive collection of HSC questions perfect to help you nail that particular topic",
+          image: questionsImage
+        },
+        {
+          title: "Analytics",
+          description: "Track your progress and see where and how you can improve",
+          image: ""
+        },
+        {
+          title: "Custom Exam Generation",
+          description: "Generate custom exams tailored to your needs and wants",
+          image: ""
         }
     ]
+
+    const [tabOrientation, setTabOrientation] = useState<string>('horizontal')
+    const [activeFeatureIndex, setActiveFeatureIndex] = useState<number>(0)
+
+
+    useEffect(() => {
+      const lgMediaQuery = window.matchMedia('(min-width: 1024px)')
+
+      function onMediaQueryChange({ matches }: { matches: boolean}) {
+        setTabOrientation(matches ? 'vertical' : 'horizontal')
+      }
+
+      onMediaQueryChange(lgMediaQuery)
+      lgMediaQuery.addEventListener('change', onMediaQueryChange)
+
+      return () => {
+        lgMediaQuery.removeEventListener('change', onMediaQueryChange)
+      }
+    }, [])
 
     return (
-        <section 
-                id="features"
-                aria-labelledby="features-title"
-                className="relative overflow-hidden bg-blue-600 pt-20 pb-28 sm:py-32">
-            <div className="relative">
-            <div className="mx-auto text-center">
-                <div className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
-                    Everything you need to master your HSC
-                </div>
-                <div className="mt-6 text-lg tracking-tight text-blue-100">
-                    Well everything except that half decent sleep schedule 
-                </div>
-            </div>
-            <div className="grid grid-cols-1 items-center gap-y-2 pt-10 sm:gap-y-6 md:mt-20 lg:pt-0">
-                {
-                    features.map((feature, index) => (
-                        <div className="group relative rounded-full py-1 px-4 lg:rounded-r-none lg:rounded-l-xl lg:p-6 hover:bg-white/10 lg:hover:bg-white/5" key={index}>
-                            <div className="font-display text-lg [&:not(:focus-visible)]:focus:outline-none text-blue-100 hover:text-white lg:text-white">
-                            <div>
-                                {feature.title}
-                            </div>
-                            <div>
-                                {feature.description}
-                            </div>
-                            </div>
-                        </div>
-                    ))
-                }
-            </div>
-            </div>
-        </section>
-    )
-}
-
-const Features = () => {
-    const features: Feature[] = [
-        {
-            title: "Exam Finding",
-            description: "We know how hard it can be looking for practice exams so we provide the largest collection of HSC and Trial exams. Our filtering features help you find what you want."
-        },
-        {
-            title: "Analytics",
-            description: "Wanting to know how you're progressing. We have you covered with analytics letting you know how your progression is coming along"
-        }
-    ]
-
-    let [tabOrientation, setTabOrientation] = useState('horizontal')
-
-  useEffect(() => {
-    let lgMediaQuery = window.matchMedia('(min-width: 1024px)')
-
-    function onMediaQueryChange({ matches }) {
-      setTabOrientation(matches ? 'vertical' : 'horizontal')
-    }
-
-    onMediaQueryChange(lgMediaQuery)
-    lgMediaQuery.addEventListener('change', onMediaQueryChange)
-
-    return () => {
-      lgMediaQuery.removeEventListener('change', onMediaQueryChange)
-    }
-  }, [])
-
-  return (
-    <section
-      id="features"
-      aria-labelledby="features-title"
-      className="relative overflow-hidden bg-blue-600 pt-20 pb-28 sm:py-32"
-    >
-      <div className="relative">
-        <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
-          <h2
-            id="features-title"
-            className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl"
-          >
-            Everything you need to run your books.
-          </h2>
-          <p className="mt-6 text-lg tracking-tight text-blue-100">
-            Well everything you need if you aren’t that picky about minor
-            details like tax compliance.
-          </p>
+      <section
+        id="features"
+        aria-labelledby="features-title"
+        className="relative overflow-hidden bg-blue-600 pt-20 pb-28 sm:py-32"
+      >
+        <div className="absolute top-0 left-0 overflow-hidden">
+          <img 
+            src={backgroundImage}
+            alt="background"
+            width={2245}
+            height={1636}
+          />
         </div>
-        <div
-          className="mt-16 grid grid-cols-1 items-center gap-y-2 pt-10 sm:gap-y-6 md:mt-20 lg:grid-cols-12 lg:pt-0">
-            <>
-              <div className="-mx-4 flex overflow-x-auto pb-4 sm:mx-0 sm:overflow-visible sm:pb-0 lg:col-span-5">
-                <div className="relative z-10 flex space-x-4 whitespace-nowrap px-4 sm:mx-auto sm:px-0 lg:mx-0 lg:block lg:space-y-1 lg:space-x-0 lg:whitespace-normal">
-                  {features.map((feature, featureIndex) => (
-                    <div
-                      key={featureIndex}
-                      className={
-                        'group relative rounded-full py-1 px-4 lg:rounded-r-none lg:rounded-l-xl lg:p-6 hover:bg-white/10 lg:hover:bg-white/5'
-                      }
-                    >
-                      <h3>
-                        <div
-                          className={
-                            'font-display text-lg [&:not(:focus-visible)]:focus:outline-none text-blue-100 hover:text-white lg:text-white'
-                        }
-                        >
-                          <span className="absolute inset-0 rounded-full lg:rounded-r-none lg:rounded-l-xl" />
-                          {feature.title}
-                        </div>
-                      </h3>
-                      <p
-                        className={'mt-2 hidden text-sm lg:block text-white'
+        <div className="relative mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
+          <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
+            <h2
+              id="features-title"
+              className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl"
+            >
+              Everything you need to master your HSC
+            </h2>
+            <p className="mt-6 text-lg tracking-tight text-blue-100">
+              Well everything except that half decent sleep schedule
+            </p>
+          </div>
+          <div
+            className="mt-16 grid grid-cols-1 items-center gap-y-2 pt-10 sm:gap-y-6 md:mt-20 lg:grid-cols-12 lg:pt-0">
+              <>
+                <div className="-mx-4 flex overflow-x-auto pb-4 sm:mx-0 sm:overflow-visible sm:pb-0 lg:col-span-5">
+                  <div className="relative z-10 flex space-x-4 whitespace-nowrap px-4 sm:mx-auto sm:px-0 lg:mx-0 lg:block lg:space-x-0 lg:whitespace-normal">
+                    {features.map((feature, featureIndex) => (
+                      <div
+                        key={featureIndex}
+                        onClick={() => setActiveFeatureIndex(featureIndex)}
+                        className={
+                          ((activeFeatureIndex === featureIndex) ? 'lg:ring-1 bg-white/10 lg:bg-white/10' : 'hover:bg-white/5 lg:hover:bg-white/5') + 
+                            ' group relative rounded-full py-1 px-4 lg:rounded-r-none lg:rounded-l-xl lg:p-6 lg:hover:ring-1 lg:ring-inset lg:ring-white/10 cursor-pointer font-display'
                         }
                       >
-                        {feature.description}
-                      </p>
+                        <h3>
+                          <div
+                            className={
+                              'text-lg [&:not(:focus-visible)]:focus:outline-none text-blue-100 hover:text-white lg:text-white'
+                          }
+                          >
+                            <span className="absolute inset-0 rounded-full lg:rounded-r-none lg:rounded-l-xl" />
+                            {feature.title}
+                          </div>
+                        </h3>
+                        <p
+                          className={((activeFeatureIndex === featureIndex) ? 'text-white' : 'text-blue-100' ) + ' mt-2 hidden text-sm lg:block hover:text-white'
+                          }
+                        >
+                          {feature.description}
+                        </p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <div className="lg:col-span-7">
+                  {features.map((feature) => (
+                    <div key={feature.title} >
+                      <div className="relative sm:px-6 lg:hidden">
+                        <div className="absolute -inset-x-4 -top-[6.5rem] -bottom-[4.25rem] bg-white/10 ring-1 ring-inset ring-white/10 sm:inset-x-0 sm:rounded-t-xl" />
+                        <p className="relative mx-auto max-w-2xl text-base text-white sm:text-center">
+                          {feature.description}
+                        </p>
+                      </div>
                     </div>
                   ))}
-                </div>
-              </div>
-              <div className="lg:col-span-7">
-                {features.map((feature) => (
-                  <div key={feature.title} >
-                    <div className="relative sm:px-6 lg:hidden">
-                      <div className="absolute -inset-x-4 -top-[6.5rem] -bottom-[4.25rem] bg-white/10 ring-1 ring-inset ring-white/10 sm:inset-x-0 sm:rounded-t-xl" />
-                      <p className="relative mx-auto max-w-2xl text-base text-white sm:text-center">
-                        {feature.description}
-                      </p>
-                    </div>
-                    <div className="relative mt-10 aspect-[1085/730] w-[45rem] overflow-hidden rounded-xl bg-slate-50 shadow-xl shadow-blue-900/20 sm:w-auto lg:mt-0 lg:w-[67.8125rem]">
+                  <div className="relative mt-10  w-[45rem] overflow-hidden rounded-xl bg-slate-50 shadow-xl shadow-blue-900/20 sm:w-auto lg:mt-0 lg:w-[67.8125rem]">
                       <img
-                        src={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAM0AAACUCAMAAAAH411kAAAA4VBMVEUTExMAAMDAwMAAwMDAAMDAwAAAwADAAAD///8AIUwyAGoJCQkdHR0AAMa+vsW7u1DEvwAAwMcAv7q1KbXAAMcBvACcAVEOFAAAAACIvXO9AXMTAAAAADuamqb19PdvYo8kAGQAIkOGhsB0dMAAGIBsbGzFxcATCxMAx8B1h8CFdMALhguGhguGCwsvBWMObGwoCk91AI9RUVEAFAAQEFG1BLUEtbVvDXLouugKHDqJwnEAxrgAyQCDhVF8hW9uh7V9drWFQ3CBMVEACwAJG1AAAC9xcnvPu8+TSKQQPz80NDTYr8BoAAABhklEQVR4nO3W6U7CQBhG4WFxwQFXlGpVXAFX1CK4b3W//wsSlcY0mXklMfoDz+nvr8nT6SSfyYrGJ4b8Tc5P5fxNzw6LZvL+ygsjorlSRmTQoEGDBg0aNGjQoEGDBg0aNGjQoEGDBg0aNGjQoEGDBg0aNGjQoEGDBg0aNGjQoEGDBg0aNGjQoEGDBg0aNGjQoEGDBg0aNGjQoEGDBg0aNGjQoEEzyJqaKCs1Z7mGv3OFufg1zZrqcld0tSW63lHdbItu90R36yqjqJmWrXuLO+ID549j/2Q9bItTbTRD4y04qqmrITWlVsX/4qhTVprIP2nCtrhxOaWxA6ZRGDRo0KBBg+ZvNCVVTxM405tNVHH1U013s1HLgNlQ3Z++9/DoHH06SfV8mOrAWfypaapeAmO7j7HWwXndVJlVVeGjxaXRPqqupBpztZ8cjswmfSGKSZ7/pJcpfF9fmuWqG+DUyKzSFPUsGjRo0KBBgwYNGjRo0KBBgwYNGjRo0KBBgwYNGjRo/qHmDTxAyCTXVsjxAAAAAElFTkSuQmCC"}
-                        width={1024}
+                        src={features[activeFeatureIndex].image}
                         alt=""
                       />
                     </div>
-                  </div>
-                ))}
-              </div>
-            </>
+                </div>
+              </>
+          </div>
         </div>
-      </div>
-    </section>
-  )
-} */
-
-const Features = () => {
-    return (
-        <div>
-            
-        </div>
+      </section>
     )
-}
+} 
 
 export default Features
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line no-undef
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -14,7 +17,11 @@ export default {
           from: { transform: 'translateX(0)' },
           to: { transform: 'translateX(-100%)' },
         }
-      }
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        display: ['Lexend', ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   plugins: [],

31c769dcd4ac54f70f97b80c6cf694a25a411ef2
Dane Urban <z5363301@ad.unsw.edu.au>
2024-07-20 14:48:05 +1000

    .

--- backend/functionality/scripts/file_transfer/local_to_idrive.py
+++ backend/functionality/scripts/file_transfer/local_to_idrive.py
@@ -7,6 +7,8 @@ from database.helpers.exam import get_exams
 def transfer_files():
     # Transfer files from local to idrive
     
+    print("Transferring files")
+
     # Go through all the exams
     exams = get_exams()
     client = _create_client()
@@ -15,10 +17,13 @@ def transfer_files():
         file_name = exam.file_location
         file_path = _get_exam_file_path(file_name)
         
-        #res = _upload_file(file_path, client, file_name)
+        res = _upload_file(file_path, client, file_name)
 
-        # Delete the file locally
-        _delete_file_locally(file_path)
+        if res:
+            # Delete the file locally
+            _delete_file_locally(file_path)
+
+    print("Files transferred")
 
 
 def _get_exam_file_path(exam_file_name: str) -> str:
@@ -31,6 +36,13 @@ def _upload_file(file_path: str, client: boto3.client, file_name: str) -> bool:
     
     Return whether operation was successful
     """
+    print(f"Uploading file {file_name}")
+
+    # Check if its already been uploaded
+    if _exam_exists(file_name, client):
+        print(f"File {file_name} already exists")
+        return False
+
     # Upload the file
     if not os.path.exists(file_path):
         print(f"File {file_path} does not exist")
@@ -41,6 +53,8 @@ def _upload_file(file_path: str, client: boto3.client, file_name: str) -> bool:
     except Exception as e:
         print(f"Error uploading file: {e}")
         return False
+    
+    print(f"File {file_name} uploaded")
     return True
 
 def _create_client() -> boto3.client:
@@ -56,3 +70,11 @@ def _create_client() -> boto3.client:
 def _delete_file_locally(file_path: str):
     # Delete the file locally
     os.remove(file_path)
+
+def _exam_exists(file_name: str, client: boto3.client) -> bool:
+    # Check if the exam exists in idrive
+    try:
+        client.get_object(Bucket="exams", Key=file_name)
+        return True
+    except Exception as e:
+        return False

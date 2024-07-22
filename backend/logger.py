2fd627fcc86fca3637c4919ea154de36cc846449
Dane Urban <d.urban@student.unsw.edu.au>
2024-07-22 19:39:25 +1000

    [~] Change backend logging

--- backend/logger.py
+++ backend/logger.py
@@ -1,26 +1,45 @@
+import datetime
 import logging
 
-# Set up logging
+# Set u logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class Logger:
     @classmethod
+    def _log_info(cls, domain: str, service: str, message: str):
+        timestamp = datetime.datetime.now()
+        
+        logger.info("\033[97m[%s %s] ts=%s msg=%s", domain.upper(), service, str(timestamp), message)
+
+    @classmethod
+    def _log_error(cls, domain: str, service: str, message: str):
+        timestamp = datetime.datetime.now()
+        
+        logger.error("\033[91m[%s %s] ts=%s msg=%s", domain.upper(), service, str(timestamp), message)
+
+    @classmethod
+    def _log_debug(cls, domain: str, service: str, message: str):
+        timestamp = datetime.datetime.now()
+        
+        logger.error("\033[93m[%s %s] ts=%s msg=%s", domain.upper(), service, str(timestamp), message)
+
+    @classmethod
     def log_backend(cls, service: str, message: str):
-        logger.info("\033[97m[BACKEND %s] %s", service, message)
+        cls._log_info("BACKEND", service, message)
 
     @classmethod
     def log_backend_error(cls, service: str, message: str):
-        logger.info("\033[91m[BACKEND %s] %s", service, message)
+        cls._log_error("BACKEND", service, message)
 
     @classmethod
     def log_database(cls, service: str, message: str):
-        logger.info("\033[97m[DATABASE %s] %s", service, message)
+        cls._log_info("DATABASE", service, message)
 
     @classmethod 
     def log_database_error(cls, service: str, message: str):
-        logger.info("\033[91m[DATABASE %s] %s", service, message)
+        cls._log_error("DATABASE", service, message)
 
     @classmethod
     def log_debug(cls, message: str):
-        logger.info("\033[93m[DEBUG] %s", message)
+        cls._log_debug("DEBUG", "", message)
